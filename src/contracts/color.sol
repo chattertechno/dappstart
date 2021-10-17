d8744571cd7383e61b4bf08db54fb413a462aa1b
chattertechno <reneekrom28@gmail.com>
2021-10-17 15:16:22 +0300

    samples for connecting

---
+++ src/contracts/color.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.5.0;
+
+import "./ERC721.sol";
+
+contract Color is ERC721Full {
+    string[] public colors;
+    mapping(string => bool) _colorExists;
+    constructor() ERC721Full ("Color", "COL") public {
+    }
+    function mint(string memory _color) public {
+        require(!_colorExists[_color]);
+        uint _id = colors.push(_color);
+        _mint(msg.sender, _id);
+        _colorExists[_color] = true;
+    }
+}
